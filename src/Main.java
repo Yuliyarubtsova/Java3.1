bd6a6ce991be99419ce2a1a1a18deeb8ee1dd20f
Yuliya Rubtsova <yuliya.rubcova89@mail.ru>
2021-01-08 20:37:08 +0800

    Added new files

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        long money = 5000095;
+        float miles = money / 20F / 100F;
+        System.out.println(miles);
+    }
+}
